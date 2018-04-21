eefa4c31e18516872ae76111ec6285ba1969b5dd
indhifarhandika <indhifarhandika@gmail.com>
2018-04-21 19:42:54 +0700

    Update

--- iTFileSharing.py
+++ iTFileSharing.py
@@ -6,6 +6,7 @@
 import SimpleHTTPServer
 import SocketServer
 import os
+from colored import fg,bg,attr
 
 ip = lambda: os.system('ifconfig | grep broadcast') #Cek IP Address
 cekfolder = lambda: os.system("pwd")
@@ -13,20 +14,21 @@ clear = lambda: os.system('clear') #"clear" untuk OS Linux | "cls" untuk OS Wind
 
 def iTools():
 	port = 99
+	print "%s" % fg('white')
 	clear()
 	ip() #Cek IP Address
-	host = raw_input('\nYour IP Address(inet) : ')#Input Ip Address, jika tidak diisi maka akan menjalankan Ip secara Otomatis
+	host = raw_input('\n%sYour IP Address(inet) : ' % fg('white'))#Input Ip Address, jika tidak diisi maka akan menjalankan Ip secara Otomatis
 	tampilan()
 	hnd = SimpleHTTPServer.SimpleHTTPRequestHandler
 	http1 = SocketServer.TCPServer((host,port),hnd)
 
 	if host == '': #Untuk Ip Otomatis
-		print '[+]Server Ip Default, Port :%s' % port
+		print '%s[+]Server Ip Default, Port :%s' % (fg('white'),port)
 		print '[+]Direktory Sharing....'
 		cekfolder()
 		print '[+]Starting.....'
 	else:
-		print '[+]Server : %s:%s' % (host,str(port)) #Ip Setting Manual
+		print '%s[+]Server : %s:%s' % (fg('white'),host,str(port)) #Ip Setting Manual
 		print '[+]Direktory Sharing....'
 		cekfolder()
 		print '[+]Starting.....'
@@ -42,6 +44,6 @@ def tampilan():
     \t\t+----=[%sEmail : indhi.farhandika@programmer.net%s]-_+
     \t\t+_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-+
     \t\t++++++++++++++++++++++++++++++++++++++++++++++++++
-    \t\t+------=[%s%shttps://indhifarhandika.github.io%s]%s""" % (attr('bold'),fg('red'),fg('white'),fg('red'),fg('white'),fg('red'),fg('white'),fg('red'),fg('white'),fg('red'),fg('white'),attr('bold'),fg('red'),fg('white'),attr('reset'))
+    \t\t+------=[%s%shttps://indhifarhandika.github.io%s]%s\n\n""" % (attr('bold'),fg('red'),fg('white'),fg('red'),fg('white'),fg('red'),fg('white'),fg('red'),fg('white'),fg('red'),fg('white'),attr('bold'),fg('red'),fg('white'),attr('reset'))
 if __name__ == "__main__":
 	iTools() #Main()
